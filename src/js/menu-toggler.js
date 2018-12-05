13a8f0f94d2d75c7029c38513f93be8d1d282b20
remzov <remzovnik@gmail.com>
2018-12-05 18:01:32 +0300

    homepage and pages

---
+++ src/js/menu-toggler.js
@@ -0,0 +1,19 @@
+var menuBtn = document.querySelector('.js-menu-btn');
+var telBtn = document.querySelector('.js-tel-btn');
+var menu = document.querySelector('.js-menu');
+var contactsBlock = document.querySelector('.js-contacts-block');
+
+menuBtn.addEventListener('click', () => {
+  if (contactsBlock.classList.contains('active')) {
+    telBtn.classList.remove('active');
+    contactsBlock.classList.remove('active');
+
+  }
+});
+
+telBtn.addEventListener('click', () => {
+  if (menu.classList.contains('active')) {
+    menuBtn.classList.remove('active');
+    menu.classList.remove('active');
+  }
+});
