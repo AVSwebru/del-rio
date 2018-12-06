e38a025db47b8cb7d3d7aceb954c6e7992206d7c
remzov <remzovnik@gmail.com>
2018-12-06 18:10:58 +0300

    cart and product

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import './uikit';
 import './menu-toggler';
 import './animations';
+import counter from './counter';
+
+//counters
+const counterMinusBtn = document.querySelectorAll('.js-counter-minus');
+const counterPlusBtn = document.querySelectorAll('.js-counter-plus');
+const counterInput = document.querySelectorAll('.js-counter-input');
+
+counter(counterMinusBtn, counterPlusBtn, counterInput);
