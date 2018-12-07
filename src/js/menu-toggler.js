ab714e05c49210c7f3d7ee38203a910cac749b56
remzov <remzovnik@gmail.com>
2018-12-07 17:57:23 +0300

    adaptation

--- src/js/menu-toggler.js
+++ src/js/menu-toggler.js
@@ -7,13 +7,24 @@ menuBtn.addEventListener('click', () => {
   if (contactsBlock.classList.contains('active')) {
     telBtn.classList.remove('active');
     contactsBlock.classList.remove('active');
-
+    // hideFocus(contactsBlock, 0);
   }
+  // (menu.classList.contains('active')) ? hideFocus(menu, 0) : hideFocus(menu, -1);
+  // menu.setAttribute('aria-hidden', !(menu.classList.contains('active')));
 });
 
 telBtn.addEventListener('click', () => {
   if (menu.classList.contains('active')) {
     menuBtn.classList.remove('active');
     menu.classList.remove('active');
+    // menu.setAttribute('aria-hidden', true);
+    // hideFocus(menu, 0);
   }
+  // (contactsBlock.classList.contains('active')) ? hideFocus(contactsBlock, 0) : hideFocus(contactsBlock, -1);
 });
+
+// function hideFocus(element, value) {
+//   element.querySelectorAll('a').forEach((link) => {
+//     link.setAttribute('tabindex', value);
+//   });
+// }
