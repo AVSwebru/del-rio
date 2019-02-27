79ae2158f5eb1ac4fb52da60150589a694edccd5
Nikita Remzov <remz@MacBook-Pro-Nikita.local>
2019-02-27 20:49:21 +0300

    corrections

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import './uikit';
 import './menu-toggler';
 import './animations';
 import counter from './counter';
+require('nunjucks');
 
+console.log(nunjucks)
 //counters
 const counterMinusBtn = document.querySelectorAll('.js-counter-minus');
 const counterPlusBtn = document.querySelectorAll('.js-counter-plus');
--- src/js/menu-toggler.js
+++ src/js/menu-toggler.js
@@ -1,44 +1,20 @@
 var menuBtn = document.querySelector('.js-menu-btn');
 var telBtn = document.querySelector('.js-tel-btn');
 var menu = document.querySelector('.js-menu');
-var menuItems = menu.querySelectorAll('a');
 var contactsBlock = document.querySelector('.js-contacts-block');
-var contactsItems = contactsBlock.querySelectorAll('a');
-
-document.addEventListener('DOMContentLoaded', () => {
-  hideFocus(menuItems, -1);
-  hideFocus(contactsItems, -1);
-});
 
 menuBtn.addEventListener('click', () => {
-  if (menu.classList.contains('active')) {
-    hideFocus(menuItems, 0);
-  } else {
-    hideFocus(menuItems, -1);
-  }
   if (contactsBlock.classList.contains('active')) {
     telBtn.classList.remove('active');
     contactsBlock.classList.remove('active');
-      hideFocus(contactsItems, -1);
   }
 });
 
 telBtn.addEventListener('click', () => {
-  if (contactsBlock.classList.contains('active')) {
-    hideFocus(contactsItems, 0);
-  } else {
-    hideFocus(contactsItems, -1);
-  }
   if (menu.classList.contains('active')) {
     menuBtn.classList.remove('active');
     menu.classList.remove('active');
-    hideFocus(menuItems, -1);
   }
 
 });
 
-function hideFocus(element, value) {
-  element.forEach((link) => {
-    link.setAttribute('tabindex', value);
-  });
-}
